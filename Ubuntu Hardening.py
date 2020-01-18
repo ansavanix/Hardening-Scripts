c6803437150e405992172b03a91735bf0d919654
egomaniacdev <coronaandrew420@gmail.com>
2020-01-17 18:22:38 -0800

    Update Ubuntu Hardening.py

--- Ubuntu Hardening.py
+++ Ubuntu Hardening.py
@@ -1,1 +1,6 @@
+import os
 print ("Ensuring mounting of cramfs filesystems is disabled")
+os.system("modprobe -n -v cramfs")
+os.system("install /bin/true")
+os.system("lsmod | grep cramfs")
+print ("If there is a cramfs device mounted run 'install crafms /bin/true' then 'rmmod cramfs'")
