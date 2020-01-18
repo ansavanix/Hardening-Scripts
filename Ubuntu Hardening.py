bb6eaff1736c45285a3058b36a2b33fe1abca15e
egomaniacdev <coronaandrew420@gmail.com>
2020-01-18 12:58:04 -0800

    Update Ubuntu Hardening.py

--- Ubuntu Hardening.py
+++ Ubuntu Hardening.py
@@ -1,15 +1,22 @@
+
+def pause():
+    programPause = raw_input("Press the <ENTER> key to continue...")
+
 import os
+os.system("clear")
 print ("Non-Automated Ubuntu Auditing System")
 print ("This script is provided as is with no warranties or guarantees what so ever")
 print ("Created by Anthony Saldana-Valle based off CIS Ubuntu 16.04 Benchmark v1.1.0")
-wait = input("PRESS ENTER TO CONTINUE...")
-print("cramfs status")
+pause()
+print("CHECKING CRAMFS STATUS...")
 os.system ("modprobe -n -v cramfs")
 os.system ("lsmod | grep cramfs")
+pause()
 print("There should be no output.")
+pause()
 print("If there is output follow the instructions below.")
 print("Create the file /etc/modprobe.d/CIS.conf if it does not already exist")
 print("Add the following line'install cramfs /bin/true'")
 print("run the command 'rmmod cramfs'")
-print("After following these instructions moutning of cramfs filesystems should be disabled")
-wait = input("PRESS ENTER TO CONTINUE...")
+print("After following these instructions mounting of cramfs filesystems should be disabled")
+pause()
