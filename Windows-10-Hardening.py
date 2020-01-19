bb97e491be63969c0e3f900050c2ecc1922cba7a
egomaniacdev <coronaandrew420@gmail.com>
2020-01-18 18:49:08 -0800

    Update Windows-10-Hardening.py

--- Windows-10-Hardening.py
+++ Windows-10-Hardening.py
@@ -1,17 +1,28 @@
 def pause():
-    programPause = raw_input("Press the <ENTER> key to continue...")
+    os.system("pause")
     
 def clear():
     os.system("cls")
     
 def cmd(command):
     os.system(command)
+    
+def netacc(sub,num):
+    print("Configuring "+ sub + ":" + num)
+    cmd("net accounts /" + sub + ":" + num)
  
 import subprocess
 import os
 clear()
+cmd("@echo off")
+cmd("color 0a")
 print ("Automated Windows 10 Auditing System")
 print ("This script is provided as is with no warranties or guarantees what so ever.")
 print ("Created by Anthony Saldana-Valle based off CIS Microsoft Windows 10 Benchmark v1.8.0")
 print ("This script should be ran as administrator to avoid issues.")
 pause()
+netacc("minpwlen","14")
+netacc("maxpwage","60")
+netacc("minpwage","1")
+netacc("uniquepw","24")
+netacc("forcelogoff","60")
